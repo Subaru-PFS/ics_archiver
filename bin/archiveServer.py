3c46eb7cf16688412f3b44ef3bed37dabec66c46
Arnaud Le Fur <alefur@lam.fr>
2017-06-13 10:12:40 +0000

    logging level change to DEBUG, max size to 1GB

--- bin/archiveServer.py
+++ bin/archiveServer.py
@@ -16,7 +16,9 @@ import socket
 import archiver.protocol
 import archiver.database
 import archiver.web
-from archiver.utils import getEnvPath
+from archiver.utils import getEnvPath, LevelFileLogObserver
+import logging
+from twisted.python.logfile import LogFile
 
 class ServerInfo(object):
     pass
@@ -40,7 +42,7 @@ def startServer(options):
     options.tmpPath = getEnvPath(options.tmpPath)
     options.listenPath = getEnvPath(options.listenPath)
     options.cmdPath = getEnvPath(options.cmdPath)
-    
+
     print 'Running',__file__,'as PID',info.pid
     if 'PID' in options.tmpPath:
         options.tmpPath = options.tmpPath.replace('PID','%d') % info.pid
@@ -52,7 +54,9 @@ def startServer(options):
     if options.interactive:
         log.startLogging(sys.stdout)
     else:
-        log.startLogging(open(os.path.join(options.tmpPath,'server.log'),'w'))
+        f = LogFile("server.log", options.tmpPath+'/', rotateLength=1000000000)
+        logger = LevelFileLogObserver(f, logging.DEBUG)
+        log.addObserver(logger.emit)
 
     # find our product dir
     info.productDir = os.getenv('ICS_ARCHIVER_DIR')
--- python/archiver/utils.py
+++ python/archiver/utils.py
@@ -1,6 +1,23 @@
 import os
-
+import logging
+from twisted.python import log
 
 def getEnvPath(path):
     path = path.split('/')
     return '/'.join([os.getenv(f[1:]) if (f and f[0] == '$') else f for f in path])
+
+class LevelFileLogObserver(log.FileLogObserver):
+
+    def __init__(self, f, level=logging.INFO):
+        log.FileLogObserver.__init__(self, f)
+        self.logLevel = level
+
+    def emit(self, eventDict):
+        if eventDict['isError']:
+            level = logging.ERROR
+        elif 'level' in eventDict:
+            level = eventDict['level']
+        else:
+            level = logging.INFO
+        if level >= self.logLevel:
+            log.FileLogObserver.emit(self, eventDict)
